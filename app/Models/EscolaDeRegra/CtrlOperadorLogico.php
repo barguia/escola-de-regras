649fbd517c2d971f5533c690b499dd5feca53338
Eduardo Purificacao <eduardo_bilico@hotmail.com>
2020-11-06 06:51:18 -0300

    Iniciado projeto: Escola de Regras

---
+++ app/Models/EscolaDeRegra/CtrlOperadorLogico.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\EscolaDeRegra;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CtrlOperadorLogico extends Model
+{
+    use HasFactory;
+}
